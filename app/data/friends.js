cea26e44f71599162a6be2b209700a2e023faac0
AnumShabbir <anumsh89@hotmail.com>
2018-09-03 21:31:45 -0400

    final files

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+module.exports = {
+	array: [
+		{
+			name: "Phoebe Buffay",
+			photo: "https://data1.ibtimes.co.in/cache-img-0-450/en/full/660948/1504621970_phoebe-buffay-friends.png",
+			scores: [2, 4, 4, 2, 4, 4, 3, 2, 4, 5]
+		},
+		{
+			name: "Joey Tribbiani",
+			photo: "https://vignette.wikia.nocookie.net/friends/images/f/f5/JoeyTribbiani.jpg/revision/latest/scale-to-width-down/350?cb=20180424154245",
+			scores: [3, 2, 1, 3, 3, 2, 4, 2, 2, 1]
+		},
+		{
+			name: "Rachel Green",
+			photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rachel-green-lipstick-friends-1503504644.jpg",
+			scores: [3, 2, 4, 5, 4, 5, 2, 3, 4, 3]
+		},
+		{
+			name: "Monica Geller",
+			photo: "https://cdn-img.instyle.com/sites/default/files/styles/684xflex/public/images/2014/WRN/050514-friends-anniversary-11-567_0.jpg",
+			scores: [5, 3, 5, 4, 3, 3, 2, 5, 5, 4]
+		},
+		{
+			name: "Chandler Bing",
+			photo: "https://vignette.wikia.nocookie.net/friends/images/2/21/2473459498_a3b4e40781.jpg/revision/latest?cb=20100601130820",
+			scores: [3, 5, 2, 3, 5, 2, 3, 3, 3, 3]
+		},
+		{
+			name: "Ross Geller",
+			photo: "https://vignette.wikia.nocookie.net/friends/images/0/0b/RossGeller.jpg/revision/latest?cb=20180424154547",
+			scores: [3, 1, 2, 2, 4, 1, 3, 4, 2, 4]
+		},
+		{
+			name: "Gunther",
+			photo: "https://i.ytimg.com/vi/lzs6Wm0xUK4/maxresdefault.jpg",
+			scores: [4, 4, 3, 2, 3, 4, 1, 4, 3, 2]
+		},
+		{
+			name: "Janice",
+			photo: "https://metrouk2.files.wordpress.com/2015/09/ad_182344509.jpg?quality=80&strip=all",
+			scores: [3, 5, 5, 3, 2, 1, 5, 2, 3, 2]
+		}
+	]
+}
+
